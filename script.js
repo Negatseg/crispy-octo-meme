84a720264a991b776d501333346bde8f33848261
Negatseg <tseganehnegash@gmail.com>
2023-09-24 17:14:03 -0500

    Update teh html and javascript to display the saved event

--- script.js
+++ script.js
@@ -9,19 +9,69 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-//const saveButtons = document.querySelectorAll('btn saveBtn col-2 col-md-1')
+document.addEventListener("DOMContentLoaded", function () {
+  updateHourStyles();
 
-//saveButtons.forEach(button => {
-  //button.addEventListener('click', function() {
-    // Traverse the DOM to find the ID of the containing time-block
-    //const timeBlock = this.closest('.time-block');
-    //const timeBlockId = timeBlock.id;
+  // Update styles every minute to account for changes in time
+  setInterval(updateHourStyles, 3600000);
 
-    // Use the ID as a key to save the user input in local storage
-    //const userInput = timeBlock.querySelector('textarea').value;
-    //localStorage.setItem(timeBlockId, userInput);
-  //});
-//});
+  function updateHourStyles() {
+      // Get the current hour
+      const currentHour = new Date().getHours();
+
+      // Loop through each time-block div
+      const timeBlocks = document.querySelectorAll(".time-block");
+      timeBlocks.forEach(function (timeBlock) {
+          const hour = parseInt(timeBlock.id.split("-")[1]);
+
+          // Remove all classes (past, present, future)
+          //timeBlock.classList.remove("past", "present", "future");
+
+          // Add the appropriate class based on the current hour
+          if (hour < currentHour) {
+              timeBlock.classList.add("past");
+          } else if (hour === currentHour) {
+              timeBlock.classList.add("present");
+          } else {
+              timeBlock.classList.add("future");
+          }
+      });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+const saveButtons = document.querySelectorAll('saveBtn')
+
+saveButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const timeBlock = this.closest('.time-block');
+    const timeBlockId = timeBlock.id;
+
+  
+    const userInput = timeBlock.querySelector('textarea').value;
+    localStorage.setItem(timeBlockId, userInput);
+    const storedText = localStorage.getItem(timeBlockId, userInput);
+    userInput.value = storedText;
+  });
+});
+
+//function displayEvents() {
+ // const eventText = timeBlock.querySelector('textarea').value;
+
+ // const events = JSON.parse(localStorage.getItem('eventText'));
+
+  //}
+
+//displayEvents();
+
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
